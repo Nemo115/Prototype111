65920a081ca3a1bc30ffb1f4f23c2fe7d8fd7be3
Nemo-5 <stickman5464@gmail.com>
2020-06-12 12:38:09 +1000

    Initial commit

---
+++ Prototype.Android/CustomEntryRenderer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Prototype;
+using Prototype.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Prototype.Droid;
+using Prototype;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace Prototype.Droid
+{
+    class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var view = (CustomEntry)Element;
+                if (view.IsCurvedCornersEnabled)
+                {
+                    //creating gradient drawable for the curved background
+                    var _gradientBackground = new GradientDrawable();
+                    _gradientBackground.SetShape(ShapeType.Rectangle);
+                    _gradientBackground.SetColor(view.BackgroundColor.ToAndroid());
+
+                    //thickness of the stroke line
+                    _gradientBackground.SetStroke(view.BorderWidth, view.BorderColor.ToAndroid());
+
+                    //Radius for the curves
+                    _gradientBackground.SetCornerRadius(DpToPixels(this.Context, Convert.ToSingle(view.CornerRadius)));
+
+                    //set the background of the label
+                    Control.SetBackground(_gradientBackground);
+                }
+                //set padding for the internal text from border
+                Control.SetPadding((int)DpToPixels(this.Context, Convert.ToSingle(22)), Control.PaddingTop, (int)DpToPixels(this.Context, Convert.ToSingle(22)), Control.PaddingBottom);
+
+            }
+        }
+
+        public static float DpToPixels(Context context, float valueInDp)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+    }
+}
