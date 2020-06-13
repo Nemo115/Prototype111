9be02fd10205ebab465be1546f8cc49c1ece5c8d
Nemo-5 <stickman5464@gmail.com>
2020-06-13 11:22:30 +1000

    .

--- Prototype/App.xaml.cs
+++ Prototype/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Prototype
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new HomePage());
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
