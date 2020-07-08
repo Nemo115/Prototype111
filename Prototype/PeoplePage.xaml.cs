545f5a6f4ec002539dc4af519c5ceb9ea10e01ec
Nemo-5 <stickman5464@gmail.com>
2020-07-08 13:33:18 +1000

    made slidable scrollviews in profile page

--- Prototype/PeoplePage.xaml.cs
+++ Prototype/PeoplePage.xaml.cs
@@ -12,6 +12,25 @@ namespace Prototype
             InitializeComponent();
             BindingContext = new PeoplePages();
         }
+
+        void PPSwiped(System.Object sender, Xamarin.Forms.SwipedEventArgs e)
+        {
+            switch (e.Direction)
+            {
+                case SwipeDirection.Up:
+
+                    ProfileBar.TranslateTo(0, -60);
+
+                    ProfileBar.FadeTo(0, 150);
+                    break;
+                case SwipeDirection.Down:
+
+                    ProfileBar.TranslateTo(0, 0);
+
+                    ProfileBar.FadeTo(1, 150);
+                    break;
+            }
+        }
     }
 
     public class DisPeoplePage
