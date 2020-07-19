8638c7112021cd18c47613d33885c972210faa81
Nemo-5 <stickman5464@gmail.com>
2020-07-19 12:14:28 +1000

    ..

--- Prototype/SearchView.xaml.cs
+++ Prototype/SearchView.xaml.cs
@@ -15,83 +15,30 @@ namespace Prototype
             InitializeComponent();
             BindingContext = new SearchViewPages();
 
-            //Startt();
-            //Position();
+            
         }
 
         
-        /*
-        public void Position()
-        {
-            if (SearchCarousel.Position == 0)
-            {
-                PeopleT.TextColor = Color.FromHex("#00BCCF");
-                TopT.TextColor = Color.FromHex("#FF8E5B");
-                PostsT.TextColor = Color.FromHex("#FF8E5B");
-                DropsT.TextColor = Color.FromHex("#FF8E5B");
-            }
-            if (SearchCarousel.Position == 1)
-            {
-                PeopleT.TextColor = Color.FromHex("#FF8E5B");
-                TopT.TextColor = Color.FromHex("#00BCCF");
-                PostsT.TextColor = Color.FromHex("#FF8E5B");
-                DropsT.TextColor = Color.FromHex("#FF8E5B");
-            }
-            if (SearchCarousel.Position == 2)
-            {
-                PeopleT.TextColor = Color.FromHex("#FF8E5B");
-                TopT.TextColor = Color.FromHex("#FF8E5B");
-                PostsT.TextColor = Color.FromHex("#00BCCF");
-                DropsT.TextColor = Color.FromHex("#FF8E5B");
-            }
-            if (SearchCarousel.Position == 3)
-            {
-                PeopleT.TextColor = Color.FromHex("#FF8E5B");
-                TopT.TextColor = Color.FromHex("#FF8E5B");
-                PostsT.TextColor = Color.FromHex("#FF8E5B");
-                DropsT.TextColor = Color.FromHex("#00BCCF");
-            }
-        }
-        */
+        
 
         void Top_Clicked(System.Object sender, System.EventArgs e)
         {
             SearchCarousel.Position = 1;
-            Top.TextColor = Color.FromHex("#00BCCF");
-            People.TextColor = Color.FromHex("#FF8E5B");
-            Posts.TextColor = Color.FromHex("#FF8E5B");
-            Drops.TextColor = Color.FromHex("#FF8E5B");
-            //Position();
         }
 
         void People_Clicked(System.Object sender, System.EventArgs e)
         {
             SearchCarousel.Position = 0;
-            Top.TextColor = Color.FromHex("#FF8E5B");
-            People.TextColor = Color.FromHex("#00BCCF");
-            Posts.TextColor = Color.FromHex("#FF8E5B");
-            Drops.TextColor = Color.FromHex("#FF8E5B");
-            //Position();
         }
 
         void Posts_Clicked(System.Object sender, System.EventArgs e)
         {
             SearchCarousel.Position = 2;
-            Top.TextColor = Color.FromHex("#FF8E5B");
-            People.TextColor = Color.FromHex("#FF8E5B");
-            Posts.TextColor = Color.FromHex("#00BCCF");
-            Drops.TextColor = Color.FromHex("#FF8E5B");
-            //Position();
         }
 
         void Drops_Clicked(System.Object sender, System.EventArgs e)
         {
             SearchCarousel.Position = 3;
-            Top.TextColor = Color.FromHex("#FF8E5B");
-            People.TextColor = Color.FromHex("#FF8E5B");
-            Posts.TextColor = Color.FromHex("#FF8E5B");
-            Drops.TextColor = Color.FromHex("#00BCCF");
-            //Position();
         }
 
         void BackArrow_Clicked(System.Object sender, System.EventArgs e)
@@ -101,7 +48,7 @@ namespace Prototype
             FilterSelect.InputTransparent = true;
             FilterButton.RotateTo(0, 200);
             FilterButton.Source = ImageSource.FromResource("Prototype.assets.Search.TopSearchTab.Filter.FilterIcons.FilterIcon3x.png");
-            //Position();
+            
         }
 
         void FilterButton_Clicked(System.Object sender, System.EventArgs e)
@@ -111,7 +58,7 @@ namespace Prototype
             FilterSelect.FadeTo(1, 200);
             FilterSelect.IsVisible = true;
             FilterSelect.InputTransparent = false;
-            //Position();
+            
         }
 
         void SearchCarousel_PositionChanged(System.Object sender, Xamarin.Forms.PositionChangedEventArgs e)
@@ -123,42 +70,47 @@ namespace Prototype
                     TopT.TextColor = Color.FromHex("#FF8E5B");
                     PostsT.TextColor = Color.FromHex("#FF8E5B");
                     DropsT.TextColor = Color.FromHex("#FF8E5B");
+
+                    Top.TextColor = Color.FromHex("#FF8E5B");
+                    People.TextColor = Color.FromHex("#00BCCF");
+                    Posts.TextColor = Color.FromHex("#FF8E5B");
+                    Drops.TextColor = Color.FromHex("#FF8E5B");
                     break;
                 case 1:
                     PeopleT.TextColor = Color.FromHex("#FF8E5B");
                     TopT.TextColor = Color.FromHex("#00BCCF");
                     PostsT.TextColor = Color.FromHex("#FF8E5B");
                     DropsT.TextColor = Color.FromHex("#FF8E5B");
+
+                    Top.TextColor = Color.FromHex("#00BCCF");
+                    People.TextColor = Color.FromHex("#FF8E5B");
+                    Posts.TextColor = Color.FromHex("#FF8E5B");
+                    Drops.TextColor = Color.FromHex("#FF8E5B");
                     break;
                 case 2:
                     PeopleT.TextColor = Color.FromHex("#FF8E5B");
                     TopT.TextColor = Color.FromHex("#FF8E5B");
                     PostsT.TextColor = Color.FromHex("#00BCCF");
                     DropsT.TextColor = Color.FromHex("#FF8E5B");
+
+                    Top.TextColor = Color.FromHex("#FF8E5B");
+                    People.TextColor = Color.FromHex("#FF8E5B");
+                    Posts.TextColor = Color.FromHex("#00BCCF");
+                    Drops.TextColor = Color.FromHex("#FF8E5B");
                     break;
                 case 3:
                     PeopleT.TextColor = Color.FromHex("#FF8E5B");
                     TopT.TextColor = Color.FromHex("#FF8E5B");
                     PostsT.TextColor = Color.FromHex("#FF8E5B");
                     DropsT.TextColor = Color.FromHex("#00BCCF");
+
+                    Top.TextColor = Color.FromHex("#FF8E5B");
+                    People.TextColor = Color.FromHex("#FF8E5B");
+                    Posts.TextColor = Color.FromHex("#FF8E5B");
+                    Drops.TextColor = Color.FromHex("#00BCCF");
                     break;
             }
         }
-
-
-
-        /*
-        bool start = true;
-
-        void Startt()
-        {
-            if (start == true)
-            {
-                SearchCarousel.Position = 1;
-            }
-        }
-        */
-
     }
 
     public class DisSearchView
