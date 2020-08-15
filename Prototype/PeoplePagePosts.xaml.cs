fcf81b487ca65585ce9def84ba08c3f471bf80f3
Nemo-5 <stickman5464@gmail.com>
2020-08-15 16:19:38 +1000

    .
    almost finished i think

--- Prototype/PeoplePagePosts.xaml.cs
+++ Prototype/PeoplePagePosts.xaml.cs
@@ -9,6 +9,8 @@ namespace Prototype
 {
     public partial class PeoplePagePosts : ContentView
     {
+        int i = 0;
+
         public PeoplePagePosts()
         {
             InitializeComponent();
@@ -19,13 +21,28 @@ namespace Prototype
         {
             var blobList = await BlobStorageService.GetBlobs<CloudBlockBlob>("teststorage1");
 
-            var firstBlob = blobList?.FirstOrDefault();
-            var photo = new PhotoModel { Title = firstBlob?.Name, Uri = firstBlob?.Uri };
-            
-            string description = photo?.Title;
-            const string V = "!&%#%#&!";
-            //string[] des = description.Split(V.ToCharArray());
-            post0.Source = ImageSource.FromUri(photo?.Uri);
+            if (blobList != null)
+            {
+                NoPosts.IsVisible = false;
+                var firstBlob = blobList?.FirstOrDefault();
+                var photo = new PhotoModel { Title = firstBlob?.Name, Uri = firstBlob?.Uri };
+
+                string description = photo?.Title;
+                const string V = "!&%#%#&!";
+                //string[] des = description.Split(V.ToCharArray());
+                post0.Source = ImageSource.FromUri(photo?.Uri);
+                foreach (var post in blobList)
+                {
+                    photoGrid.Children.Add(new Image
+                    {
+                        Source = ImageSource.FromUri(photo?.Uri)
+                    });
+                }
+            }
+            else
+            {
+                NoPosts.IsVisible = true;
+            }
         }
     }
 }
