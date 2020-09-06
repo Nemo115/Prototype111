2e1d37f84b934d3d8ef1d6685f82832376a11c4b
Nemo-5 <stickman5464@gmail.com>
2020-09-07 08:25:17 +1000

    .

--- Prototype/PeoplePageCustomizePage.xaml.cs
+++ Prototype/PeoplePageCustomizePage.xaml.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
 
 namespace Prototype
 {
     public partial class PeoplePageCustomizePage : ContentView
     {
+        public MediaFile profilePic;
+
         public PeoplePageCustomizePage()
         {
             InitializeComponent();
+        }
+
+        async void ChangeProfilePic_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                return;
+            }
+            else
+            {
+                var mediaOption = new PickMediaOptions()
+                {
+                    PhotoSize = PhotoSize.Medium;
+                }
+            }
         }
     }
 }
